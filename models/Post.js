a779cbccf2e8e04018c1c6feca913273fe918c71
Vk2533120 <vk2533120@gmail.com>
2025-09-11 00:30:10 +0530

    2nd commit

--- models/Post.js
+++ models/Post.js
@@ -1,21 +1,10 @@
 const mongoose = require("mongoose");
 
-const commentSchema = new mongoose.Schema({
-  user: { type: String, required: true },
-  text: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now }
-});
-
-const postSchema = new mongoose.Schema(
-  {
-    title: { type: String, required: true },
-    content: { type: String, required: true },
-    author: { type: String, default: "Anonymous" },
-    image: { type: String }, // Store image URL or filename
-    likes: { type: Number, default: 0 },
-    comments: [commentSchema],
-  },
-  { timestamps: true }
-);
+const postSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  author: { type: String },
+  image: { type: String }, // store image URL here
+}, { timestamps: true });
 
 module.exports = mongoose.model("Post", postSchema);
